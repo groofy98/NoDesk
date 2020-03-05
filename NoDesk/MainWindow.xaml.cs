9b16fef42a69b09418a0c4af1803afe43ab547c8
Sjors Grooff <s.grooff@student.tudelft.nl>
2020-03-05 15:32:15 +0100

    sample db conn

--- NoDesk/MainWindow.xaml.cs
+++ NoDesk/MainWindow.xaml.cs
@@ -12,9 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace NoDesk
 {
+    //"mongodb+srv://Sjors:*FDmqwry+r3rT+i@cluster0-dg3ym.mongodb.net/test?retryWrites=true&w=majority"
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,6 +26,26 @@ namespace NoDesk
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+            var dbClient = new MongoClient("mongodb+srv://Sjors:Yolo1@cluster0-dg3ym.mongodb.net/test?retryWrites=true&w=majority");
+            var dbList = dbClient.ListDatabases().ToList();
+            var database = dbClient.GetDatabase("sample_training");
+            var collection = database.GetCollection<BsonDocument>("grades");
+            var firstDocument = collection.Find(new BsonDocument()).FirstOrDefault();
+            Console.WriteLine(firstDocument.ToString());
+            Console.WriteLine("The list of databases are:");
+
+            foreach (var item in dbList)
+            {
+                Console.WriteLine(item);
+            }
+
+
         }
     }
 }
