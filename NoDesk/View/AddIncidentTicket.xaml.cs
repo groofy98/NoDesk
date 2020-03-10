c021cf6266ee7949321f568a204cde7a749b703d
Dane Port <daneport2@hotmail.com>
2020-03-10 11:16:17 +0100

    Add Ticket UI

---
+++ NoDesk/View/AddIncidentTicket.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NoDesk
+{
+    /// <summary>
+    /// Interaction logic for AddIncidentTicket.xaml
+    /// </summary>
+    public partial class AddIncidentTicket : Page
+    {
+        public AddIncidentTicket()
+        {
+            InitializeComponent();
+        }
+
+        private void IncidentDate_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
