8af5e8f5117703f922c4f5a90f74f817b6837c4a
DanePort <daneport2@hotmail.com>
2020-03-23 10:36:20 +0100

    ADd ticketUI & Model

---
+++ NoDesk/Model/IncidentTicket.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoDesk.model
+{
+    public enum IncidentType{Software, Hardware, Service}
+    public class IncidentTicket
+    {
+        public ObjectId incidentId { get; set; }
+        public BsonDateTime incidentDate { get; set; }
+        public IncidentType incidentType { get; set; }
+        public string reportedBy { get; set; }
+        public int priotity { get; set; }
+        public int incidentDeadline { get; set; }
+
+        public BsonDocument CreateBson()
+        {
+            var document = new BsonDocument {
+                {"incident_id", this.incidentId },
+                {"date", this.incidentDate },
+                {"type", this.incidentType },
+                {"Reported by", this.reportedBy },
+                {"Priority", this.priotity },
+                {"Deadline", this.incidentDeadline }
+            };
+
+            return document;
+        }
+    }
+
+    
+}
