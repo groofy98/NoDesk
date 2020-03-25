bc2fec7c21d2c4e541a4f80f97aac7feb3d4b998
Lidda <amblink@hotmail.com>
2020-03-25 11:15:48 +0100

    Made LoginView Layout

---
+++ NoDesk/ViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoDesk.ViewModels {
+    public class LoginViewModel : Conductor<Object> {
+    }
+}
